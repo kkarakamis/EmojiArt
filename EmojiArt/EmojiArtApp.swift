5621b695920c490f1c7a07c810db012e0c41d762
Kutay Karakamış <32245278+kkarakamis@users.noreply.github.com>
2021-12-09 12:09:10 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Kutay Karakamış on 9.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
