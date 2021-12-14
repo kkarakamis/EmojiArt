b7f09eef517149bfd2ef50d3138b1848a580036d
Kutay Karakamış <32245278+kkarakamis@users.noreply.github.com>
2021-12-14 14:53:05 +0300

    palette chooser view added

--- EmojiArt/Extensions+Stanford/UtilityExtensions.swift
+++ EmojiArt/Extensions+Stanford/UtilityExtensions.swift
@@ -78,14 +78,12 @@ extension Set where Element: Identifiable {
 // (thus withNoRepeatedCharacters below)
 
 extension String {
-    var withNoRepeatedCharacters: String {
-        var uniqued = ""
-        for ch in self {
-            if !uniqued.contains(ch) {
-                uniqued.append(ch)
+    var removingDuplicateCharacters: String {
+        reduce(into: "") { sofar, element in
+            if !sofar.contains(element) {
+                sofar.append(element)
             }
         }
-        return uniqued
     }
 }
 
